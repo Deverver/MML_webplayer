aa842a833876da9e329b62244706aa37d5b8eb12
Deverver <jackRMrasmussen@gmail.com>
2025-10-27 17:37:04 +0100

    added routes and controllers dir's along the associated js files songController.js & songRoutes.js.
    Added vitest & added test dir, along server.test.js

---
+++ test/server.test.js
@@ -0,0 +1,29 @@
+// tests/app.test.js
+import request from "supertest";
+import { describe, it, expect } from "vitest";
+import server from "../src/server.js";
+
+/*
+  We’re testing that our Express routes and controllers:
+  1. Respond with HTTP 200 status
+  2. Return the correct message in JSON
+*/
+
+describe("Express App Routes", () => {
+    it("GET / should return the songs page", async () => {
+        const {melodyOnly, melodyPlusHarmony, fullSongs} = await request(server).get("/songs");
+        const res = await fetch("/songs");
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            categorized:{melodyOnly, melodyPlusHarmony, fullSongs}
+        });
+    });
+
+    it("GET /songs/songIndex should return specific song data", async () => {
+        const response = await request(server).get("/songs/4");
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            message: "About Us - We build amazing apps!",
+        });
+    });
+});
